9549a4c85d69ee0d4ff461905f1c2b1bbdca62bb
Linmiao Xu <linmiao.xu@gmail.com>
2017-08-07 03:29:19 -0700

    Improve the way annotation previews are presented

--- app/javascript/components/annotation_previews.vue
+++ app/javascript/components/annotation_previews.vue
@@ -1,7 +1,9 @@
-<template>
-  <div class="annotation-previews">
-    <div class="annotation-preview" v-for="(annotation, i) in annotationPreviews">
-      <annotation :annotation="annotation"/>
+<template lang="pug">
+  .annotation-previews
+    .preview(v-for="(annotation, i) in annotationPreviews")
+      span.username {{ annotation.username }}
+      span.move-string {{ annotation.move_string }}
+      span.text {{ annotation.text }}
     </div>
   </div>
 </template>
@@ -35,15 +37,28 @@
   }
 </script>
 
-<style>
-  .annotation-preview {
-    font-size: 12px;
-    overflow: hidden;
-    white-space: nowrap;
-    text-overflow: ellipsis;
-    margin: 5px 0 10px;
-    color: #005F2F;
-    opacity: 0.9;
-    clear: left;
-  }
+<style lang="stylus">
+  .preview
+    font-size 12px
+    overflow hidden
+    white-space nowrap
+    text-overflow ellipsis
+    margin 5px 0 10px
+    color #333
+    opacity 0.9
+    clear left
+
+    .username
+      font-weight bold
+
+    .move-string
+      margin 0 10px 0 7px
+      opacity 0.7
+
+    .text
+      opacity 0.5
+
+    &:last-child
+      margin-bottom 0
+
 </style>
