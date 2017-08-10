f23f394d06c5bd3f3f377e5f4972fa3a00a86510
Linmiao Xu <linmiao.xu@gmail.com>
2017-08-10 03:56:50 -0700

    Use login page styles for sign up page

--- app/javascript/routes/sign_up.vue
+++ app/javascript/routes/sign_up.vue
@@ -1,12 +1,15 @@
-<template>
-  <main id="sign_up">
-    <form @submit="submitCredentials">
-      <input type="text" placeholder="Username" ref="username"/>
-      <input type="password" placeholder="Password" ref="password"/>
-      <input type="password" placeholder="Confirm password" ref="password_confirmation"/>
-      <input type="submit" value="Sign up"/>
-    </form>
-  </main>
+<template lang="pug">
+  main#sign_up
+    h1 Sign up
+    form(@submit="submitCredentials")
+      input(type="text" ref="username" placeholder="Username"
+            @keyup="hideError")
+      input(type="password" ref="password" placeholder="Password"
+            @keyup="hideError")
+      div
+        input(type="submit" value="Sign up")
+        .error(v-if="showError") Invalid username or password
+
 </template>
 
 <script>
@@ -16,8 +19,17 @@
   export default {
     beforeRouteEnter: requireAnonymous,
 
+    data() {
+      return {
+        showError: false,
+      }
+    },
+
     methods: {
-      submitCredentials: function(e) {
+      hideError() {
+        this.showError = false
+      },
+      submitCredentials(e) {
         e.preventDefault()
         const credentials = {
           user: {
@@ -28,9 +40,53 @@
         }
         createUser(credentials).then(() => {
           getUserInfo()
-          window.location = "/"
+          router.push({ path: '/' })
+        }).catch(() => {
+          this.showError = true
         })
       }
     },
   }
 </script>
+
+<style lang="stylus" scoped>
+  main#sign_up
+    margin 100px auto
+    width 600px
+
+  h1
+    font-size 20px
+    margin-bottom 30px
+
+  div
+    display flex
+    align-items center
+
+  input
+    display block
+    font-size 18px
+
+    &[type="text"], &[type="password"]
+      border-radius 2px
+      border 1px solid #e0e0e0
+      padding 7px 14px
+      margin 15px 0
+      width 320px
+
+    &[type="submit"]
+      color white
+      background rgb(58, 137, 201)
+      border none
+      border-radius 2px
+      font-size 18px
+      padding 7px 24px
+
+      &:hover
+        opacity 0.9
+        cursor pointer
+
+  .error
+    color orange
+    margin-left 20px
+
+</style>
