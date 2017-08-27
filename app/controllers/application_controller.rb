3f97b72fe630983361502a5fd559d407e03d9327
Linmiao Xu <linmiao.xu@gmail.com>
2017-08-26 21:56:47 -0700

    Don't render games into html on homepage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  def home
-    @games = Game.order('id DESC').limit(10)
-  end
-
   def index
   end
 end
