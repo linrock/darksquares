eb280a8248e646d4e879ce2ce7a404d5429724dc
Linmiao Xu <linmiao.xu@gmail.com>
2017-09-05 00:52:24 -0700

    Configure stockfish options using env variables

--- app/models/stockfish_analysis.rb
+++ app/models/stockfish_analysis.rb
@@ -6,12 +6,20 @@ class StockfishAnalysis
     @output = nil
   end
 
+  def n_threads
+    (ENV["STOCKFISH_THREADS"] || 2).to_i
+  end
+
+  def hash_table_size
+    (ENV["STOCKFISH_HASH_SIZE"] || 4096).to_i
+  end
+
   def analyze!
     engine = Stockfish::Engine.new
     multipv = @options[:multipv].to_i
     engine.multipv(multipv) if multipv > 0
-    engine.execute "setoption name Threads value 2"
-    engine.execute "setoption name Hash value 4096"
+    engine.execute "setoption name Threads value #{n_threads}"
+    engine.execute "setoption name Hash value #{hash_table_size}"
     @output = engine.analyze @fen, { :depth => @options[:depth] || 10 }
     engine.execute "quit"
     @output
