5ab1a05fdae82388d19f3eb8159aa662e4bad310
Linmiao Xu <linmiao.xu@gmail.com>
2017-09-11 13:41:56 -0700

    Use localStorage directly for persisting local state

--- app/javascript/store/local_storage.js
+++ app/javascript/store/local_storage.js
@@ -1,12 +1,14 @@
 // For data that's meant to persist across sessions on the client
 
-import store from 'store'
+// import store from 'store'
+
+const store = window.localStorage
 
 const ACCESS_TOKEN_KEY = `access_token`
 const USERNAME_KEY = `username`
 
-export const getAccessToken = () => store.get(ACCESS_TOKEN_KEY)
-export const setAccessToken = token => store.set(ACCESS_TOKEN_KEY, token)
+export const getAccessToken = () => store.getItem(ACCESS_TOKEN_KEY)
+export const setAccessToken = token => store.setItem(ACCESS_TOKEN_KEY, token)
 
-export const getUsername = () => store.get(USERNAME_KEY)
-export const setUsername = username => store.set(USERNAME_KEY, username)
+export const getUsername = () => store.getItem(USERNAME_KEY)
+export const setUsername = username => store.setItem(USERNAME_KEY, username)
