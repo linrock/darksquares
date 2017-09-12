f5cb07d267608f6da88d595bcb9549d70d5bbba8
Linmiao Xu <linmiao.xu@gmail.com>
2017-09-11 20:21:34 -0700

    Remove items in local storage when set to falsy values

--- app/javascript/store/local_storage.js
+++ app/javascript/store/local_storage.js
@@ -8,7 +8,19 @@ const ACCESS_TOKEN_KEY = `access_token`
 const USERNAME_KEY = `username`
 
 export const getAccessToken = () => store.getItem(ACCESS_TOKEN_KEY)
-export const setAccessToken = token => store.setItem(ACCESS_TOKEN_KEY, token)
+export const setAccessToken = token => {
+  if (token) {
+    store.setItem(ACCESS_TOKEN_KEY, token)
+  } else {
+    store.removeItem(ACCESS_TOKEN_KEY)
+  }
+}
 
 export const getUsername = () => store.getItem(USERNAME_KEY)
-export const setUsername = username => store.setItem(USERNAME_KEY, username)
+export const setUsername = username => {
+  if (username) {
+    store.setItem(USERNAME_KEY, username)
+  } else {
+    store.removeItem(USERNAME_KEY)
+  }
+}
