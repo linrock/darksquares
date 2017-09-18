0c538a23f45ec2af4a5bd014df344426f91144a9
Linmiao Xu <linmiao.xu@gmail.com>
2017-09-18 13:21:48 -0700

    Fix sometimes-incorrect graph loading message when analysis completes

--- app/models/game_analysis_status.rb
+++ app/models/game_analysis_status.rb
@@ -17,8 +17,11 @@ class GameAnalysisStatus
   end
 
   def status
+    if percent_analyzed == 100
+      return ANALYSIS_STATUS[:complete] if @game.graph_points.present?
+      return ANALYSIS_STATUS[:in_progress]
+    end
     return ANALYSIS_STATUS[:pending] if n_workers == 0 || percent_analyzed == 0
-    return ANALYSIS_STATUS[:complete] if percent_analyzed == 100
     ANALYSIS_STATUS[:in_progress]
   end
 
