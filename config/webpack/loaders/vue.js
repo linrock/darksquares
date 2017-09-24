2d520057fd94865a8a356be0e557e8b45409f8c9
Linmiao Xu <linmiao.xu@gmail.com>
2017-09-24 16:33:18 -0700

    Extract vue component css into a separate css pack file

--- config/webpack/loaders/vue.js
+++ config/webpack/loaders/vue.js
@@ -3,12 +3,5 @@ module.exports = {
   loader: 'vue-loader',
   options: {
     extractCSS: true,
-    loaders: {
-      js: 'babel-loader',
-      file: 'file-loader',
-      scss: 'vue-style-loader!css-loader!postcss-loader!sass-loader',
-      sass: 'vue-style-loader!css-loader!postcss-loader!sass-loader?indentedSyntax',
-      stylus: 'vue-style-loader!css-loader!postcss-loader!stylus-loader',
-    }
   }
 }
